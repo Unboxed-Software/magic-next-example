c219d376958be809ab818532df40ca8a548b062c
PBillingsby <philbillingsby@gmail.com>
2023-12-14 17:25:31 -0500

    add tailwind bg style instead of css style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import SendTransaction from "./components/SendTransaction"
 export default function Home() {
   const { user } = useUser()
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-black">
       {user ? (
         <div className="p-2 flex flex-col w-[40vw] mx-auto">
           <WalletDetail />
